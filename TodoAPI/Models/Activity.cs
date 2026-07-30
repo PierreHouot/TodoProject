4c1fea36e74f657f7b418e9301d08e9d5702d2f2
pierrehouot <lightpower@hotmail.fr>
2026-07-30 15:33:50 +0200

    update activity definition

--- TodoAPI/Models/Activity.cs
+++ TodoAPI/Models/Activity.cs
@@ -1,6 +1,6 @@
 ﻿namespace TodoAPI.Models
 {
-    public class Activity
+    public record Activity
     {
         public required string Id { get; set; }
         public string? Name { get; set; }
